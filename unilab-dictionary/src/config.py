dc21cbe9c41810c9efbc4b6b8ff223ef5908d91d
Levan Kolbaia <125897257+Rizznado@users.noreply.github.com>
2023-06-23 18:49:26 +0400

    Delete unilab-dictionary directory

--- unilab-dictionary/src/config.py
+++
@@ -1,9 +0,0 @@
-from os import path
-
-
-class Config(object):
-    BASE_DIRECTORY = path.abspath(path.dirname(__file__))
-    UPLOAD_PATH = path.join(BASE_DIRECTORY, "uploads")
-
-    SECRET_KEY = "secret_key"
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + path.join(BASE_DIRECTORY, "database.db")
